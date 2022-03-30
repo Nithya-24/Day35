754043e869d1e5272ec2a704bc57e6fc6bdee6d3
Nithya-24 <nithyaramadoz@gmail.com>
2022-03-30 22:55:19 +0530

    Retrieve the Employee Data from Database

--- src/test/java/Emp_PayrollTest.java
+++ src/test/java/Emp_PayrollTest.java
@@ -1,11 +1,45 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import java.util.List;
+
+import com.EmpPayrollServiceDB;
+import com.EmpPayrollServiceDB.IOService;
+import com.EmployeePayrollData;
 
 public class Emp_PayrollTest {
 
 	@Test
 	public void shouldAnswerWithTrue() {
 		assertTrue(true);
+	}
+	public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmp = { new EmployeePayrollData(1, "Nithya R", 400000),
+				new EmployeePayrollData(2, "XYZ", 500000),
+				new EmployeePayrollData(3, "ABC", 600000) };
+		EmpPayrollServiceDB employeePayrollService;
+		employeePayrollService = new EmpPayrollServiceDB(Arrays.asList(arrayOfEmp));
+		employeePayrollService.writeEmployeeData(IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+		employeePayrollService.printData(IOService.FILE_IO);
+		List<EmployeePayrollData> employeeList = employeePayrollService.readData(IOService.FILE_IO);
+		System.out.println(employeeList);
+		assertEquals(3, entries);
+	}
+
+	@Test
+	/**
+	 * created test method to match the employeeCount
+	 */
+	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+
+		/**
+		 * Creating object for EmployeePayRollService Class
+		 */
+		EmpPayrollServiceDB employeePayrollService = new EmpPayrollServiceDB();
+		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readData(IOService.DB_IO);
+		assertEquals(3, employeePayrollData.size());
 	}
 }
